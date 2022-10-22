6ee0a9f25481ffffb776e588bc30b99274ffe4f9
Stephen Downes <stephen@downes.ca>
2022-10-22 19:41:42 -0400

    Update bookmarklet.js
    Corrects selection for indieweb authorname, to ensure it is found within an hcard

--- bookmarklet.js
+++ bookmarklet.js
@@ -14,15 +14,18 @@ if (document.querySelector('link[rel="license"]')) {
 }
 
 if (document.getElementById('hCard')) {
-alert('I have a hit');
+alert('I have an hCard hit');
    hcard = document.getElementById('hCard').innerHTML
 }
 
-
-if (document.getElementsByClassName('p-name')) {
-   pname = document.getElementsByClassName('p-name')[0].innerHTML
+if (document.getElementsByClassName('h-card')[0]) {
+   if (document.getElementsByClassName('h-card')[0].getElementsByClassName('p-name')[0]) {
+      pnameraw = document.getElementsByClassName('h-card')[0].getElementsByClassName('p-name')[0].innerHTML
+      pname = pnameraw.replace(/<\/?[^>]+(>|$)/g, "");
+   alert(pname);
+   }
 }
 
 
-newUrl = `https://www.downes.ca/cgi-bin/admin.cgi?action=hub&title=${title}&url=${currentUrl}&selected=${text}&description=${description}&icon=${icon}&license=${license}&hcard=${hcard}&pname=${pname}`;
+newUrl = `https://www.downes.ca/cgi-bin/api.cgi?cmd=edit&id=new&table=post&hub=yes&title=${title}&url=${currentUrl}&link=${currentUrl}&description=${description}${text}&icon=${icon}&license=${license}&hcard=${hcard}&author=${pname}`;
 window.open(newUrl,"", "width=400, height=600");
